dcbc0317d12daa50274f33218f3af956b91de2f7
NguyenGiaKiet2004 <nguyengiakietcm2018@gmail.com>
2025-05-28 15:23:22 +0700

    Dev - HomeScreen and InfosmationFilm by Gia Kiet

--- app/src/main/java/com/example/appmoview/MainActivity.kt
+++ app/src/main/java/com/example/appmoview/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appmoview.presentation.screens.RegisterScreen
+import com.example.appmoview.presentation.screens.HomeScreen
+import com.example.appmoview.presentation.screens.HomeScreen1
+import com.example.appmoview.presentation.screens.InformationFilmScreen
 import com.example.appmoview.presentation.theme.SystemTheme
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent{
             SystemTheme{
-                RegisterScreen()
+                InformationFilmScreen()
             }
         }
 
